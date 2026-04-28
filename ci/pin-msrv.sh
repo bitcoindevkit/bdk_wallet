da4c4ba34dcdd7019524318ce7a8088f42053aaf
Steve Myers <steve@notmandatory.org>
2026-04-28 12:57:00 -0500

    ci: remove examples from workspace, fix pinned deps

--- ci/pin-msrv.sh
+++ ci/pin-msrv.sh
@@ -10,14 +10,15 @@ set -euo pipefail
 # cargo clean
 # rustup override set 1.63.0
 
-cargo update -p clap --precise "4.5.17"
+cargo update -p libc --precise "0.2.183"
+cargo update -p getrandom@0.4.2 --precise 0.3.4
 cargo update -p once_cell --precise "1.20.3"
-cargo update -p syn --precise "2.0.106"
 cargo update -p quote --precise "1.0.41"
 cargo update -p serde_json --precise "1.0.145"
 cargo update -p anyhow --precise "1.0.100"
 cargo update -p tempfile --precise "3.25.0"
 cargo update -p proc-macro2 --precise "1.0.103"
 cargo update -p ryu --precise "1.0.20"
+cargo update -p syn --precise "2.0.106"
 cargo update -p itoa --precise "1.0.15"
 cargo update -p unicode-ident --precise "1.0.22"
